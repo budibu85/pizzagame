2b3b0179fc6b9de12c9e8fc2acb920cc4cd2d7ec
fulvio.chiarelli <fulvio.chiarelli@FCHIARELLI-NEW>
2022-06-23 20:17:09 +0200

    Add project files.

---
+++ BudiBu85.PizzaGame/Program.cs
@@ -0,0 +1,10 @@
+using BudiBu85.PizzaGame;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
